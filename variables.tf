d5acae976ae49d9fd265ad73b79f688d8f153cca
Khue Doan <khuedoan98@gmail.com>
2021-03-07 13:34:56 +0700

    Less hard coded values

--- variables.tf
+++ variables.tf
@@ -1,3 +1,31 @@
-variable "shape" {
+variable "instance_shape" {
   default = "VM.Standard.E2.1.Micro"
+}
+
+variable "ssh_public_key" {
+
+}
+
+variable "compartment_id" {
+
+}
+
+variable "user_ocid" {}
+variable "tenancy_ocid" {}
+variable "fingerprint" {}
+variable "region" {}
+variable "private_key_path" {}
+
+variable "instance_image_id" {
+  default = "ocid1.image.oc1.us-sanjose-1.aaaaaaaan4g4q527bljtyczck6xrsutbzps6h7mut2xcfhnbzw66sbbsvwoq"
+}
+
+variable "vcn_cidr_blocks" {
+	default = [
+		"10.0.0.0/16"
+	]
+}
+
+variable "subnet_cidr_block" {
+	default = "10.0.0.0/24"
 }
