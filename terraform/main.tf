ee4455ecd253f3f24ff2903bc3c32508f2ae0feb
Khue Doan <khuedoan98@gmail.com>
2021-04-04 22:46:54 +0700

    Provisioner for the instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,34 @@ resource "tls_private_key" "ssh" {
   ecdsa_curve = "P256"
 }
 
+resource "local_file" "ssh_private_key" {
+  content         = tls_private_key.ssh.private_key_pem
+  filename        = "${path.module}/private.pem"
+  file_permission = "0600"
+}
+
 resource "oci_core_instance" "instance" {
-  availability_domain = "gHLA:US-SANJOSE-1-AD-1"
+  availability_domain = "gHLA:US-SANJOSE-1-AD-1" # TODO
   compartment_id      = var.compartment_id
+  shape               = var.instance_shape
+
+  instance_options {
+    are_legacy_imds_endpoints_disabled = true
+  }
+
   create_vnic_details {
     assign_public_ip = "true"
     subnet_id        = oci_core_subnet.subnet.id
   }
-  instance_options {
-    are_legacy_imds_endpoints_disabled = true
-  }
-  metadata = {
-    ssh_authorized_keys = tls_private_key.ssh.public_key_openssh
-    user_data           = ""
-  }
-  shape = var.instance_shape
+
   source_details {
     source_type = "image"
     source_id   = var.instance_image_id
+  }
+
+  metadata = {
+    ssh_authorized_keys = tls_private_key.ssh.public_key_openssh
+    user_data           = ""
   }
 }
 
@@ -48,4 +58,15 @@ resource "oci_core_default_route_table" "default_route_table" {
     network_entity_id = oci_core_internet_gateway.internet_gateway.id
   }
   manage_default_resource_id = oci_core_vcn.vcn.default_route_table_id
+}
+
+resource "null_resource" "ansible" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/../ansible"
+    command     = "ls -la"
+  }
 }
